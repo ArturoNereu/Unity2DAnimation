b20f327c556173d803c33d20fa53aadf322c60e7
Arturo Nereu <hdgam3r@gmail.com>
2017-05-04 19:20:42 -0700

    Word switching now working

---
+++ Anim2DDemo/Assets/Scripts/SwitchWeapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchWeapon : MonoBehaviour
+{
+    private Animator animator;
+
+    private int weaponIndex = 0;
+
+    [SerializeField]
+    private GameObject[] weapons;
+
+    [SerializeField]
+    private GameObject handBone;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Attack");
+
+            if(handBone)
+            {
+                weapons[weaponIndex].transform.SetParent(null);
+                weapons[weaponIndex].transform.position = new Vector3(-10, 0, 0);
+
+                weaponIndex++;
+                weaponIndex = weaponIndex % weapons.Length;
+          
+                weapons[weaponIndex].transform.SetParent(handBone.transform);
+                weapons[weaponIndex].transform.localPosition = Vector3.zero;
+            }
+        }
+	}
+}
